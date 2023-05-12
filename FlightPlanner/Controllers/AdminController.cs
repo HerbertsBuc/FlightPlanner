a08fb88e44e01b00c5645ee2552b32f6969ced43
herberts.bucenieks@gmail.com <herberts.bucenieks@gmail.com>
2023-05-12 08:22:11 +0300

    Completed 18 of 19 tests from first iteration

---
+++ FlightPlanner/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+            if (flight == null)
+                return NotFound();
+
+            return Ok(flight);
+        }
+
+        [HttpPut]
+        [Route("flights")]
+        public IActionResult AddFlight(Flight flight)
+        {
+            if (FlightStorage.FlightHasNullValues(flight))
+                return BadRequest();
+
+            if (FlightStorage.SameAirport(flight))
+                return BadRequest();
+
+            if (FlightStorage.ArrivalBeforeDeparture(flight))
+                return BadRequest();
+
+            if (FlightStorage.FlightExists(flight))
+                return Conflict();
+
+            FlightStorage.AddFlight(flight);
+
+            return Created("", flight);
+        }
+
+        [HttpDelete]
+        [Route("flights/{id}")]
+        public IActionResult DeleteFlight(int id)
+        {
+            FlightStorage.DeleteFlight(id);
+
+            return Ok();
+        }
+
+    }
+}
